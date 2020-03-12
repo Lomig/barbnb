8312a56bbec49a3a21ffa0435c9e80480cc3f6d6
Lomig <dunwael@gmail.com>
2020-03-13 00:05:13 +0100

    Mapbox is now controlled through StimulusJS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,13 +26,12 @@ import "controllers";
 import "custom/button";
 import { initAutocomplete } from "custom/init_autocomplete";
 import { initAutocompleteSearch } from "custom/init_autocomplete";
-import { initMapbox } from '../custom/init_mapbox';
-import { initSlick } from '../custom/carousel';
+// import { initMapbox } from '../custom/init_mapbox';
+import { initSlick } from "../custom/carousel";
 
-document.addEventListener('turbolinks:load', () => {
-  initMapbox();
+document.addEventListener("turbolinks:load", () => {
+  // initMapbox();
   initAutocomplete();
   initAutocompleteSearch();
   initSlick();
-})
-
+});
