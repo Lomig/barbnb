afcbc0a26568db04acaccb41d99cf13065b28914
Lomig <dunwael@gmail.com>
2020-03-14 18:14:29 +0100

    Move assets to simplify path. Fix map CSS

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -7,14 +7,9 @@ require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
-require.context("../assets/images", true);
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+const images = require.context("../images", true);
+const imagePath = name => images(name, true);
 
 // ----------------------------------------------------
 // Note(lewagon): ABOVE IS RAILS DEFAULT CONFIGURATION
