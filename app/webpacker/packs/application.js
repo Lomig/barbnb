8f14e3b669b83eb302a67ea924d67b776f17592c
Lomig <dunwael@gmail.com>
2020-03-14 16:46:31 +0100

    Transitioned from Sprockets to Webpacker

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
+require.context("../assets/images", true);
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -19,9 +20,6 @@ require("channels");
 // Note(lewagon): ABOVE IS RAILS DEFAULT CONFIGURATION
 // WRITE YOUR OWN JS STARTING FROM HERE 👇
 // ----------------------------------------------------
-
-import "slick-carousel/slick/slick.scss";
-import "slick-carousel/slick/slick-theme.scss";
 
 import "bootstrap";
 import "controllers";
